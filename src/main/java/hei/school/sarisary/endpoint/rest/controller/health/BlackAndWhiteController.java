36f07bbecda9d27fff54c881c436085d28218247
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-22 12:16:55 +0300

    black and white controlleur

---
+++ src/main/java/hei/school/sarisary/endpoint/rest/controller/health/BlackAndWhiteController.java
@@ -0,0 +1,54 @@
+package hei.school.sarisary.endpoint.rest.controller.health;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/black-and-white")
+public class BlackAndWhiteController {
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> uploadImage(@PathVariable String id, @RequestParam("file") MultipartFile file) {
+        try {
+            byte[] imageBytes = file.getBytes();
+            String transformedImageUrl = ConvertBlackWhite.convertToBlackAndWhite(imageBytes, id);
+            return ResponseEntity.ok().build();
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Map<String, String>> getImageUrls(@PathVariable String id) {
+        String transformedImageUrl = ConvertBlackWhite.getTransformedImageUrl(id);
+        Map<String, String> response = new HashMap<>();
+        response.put("transformed_url", transformedImageUrl);
+        return ResponseEntity.ok(response);
+    }
+
+    static class ConvertBlackWhite {
+        public static String convertToBlackAndWhite(byte[] imageBytes, String id) throws IOException {
+            ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+            BufferedImage image = ImageIO.read(bis);
+            BufferedImage blackAndWhiteImage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+            blackAndWhiteImage.getGraphics().drawImage(image, 0, 0, null);
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ImageIO.write(blackAndWhiteImage, "png", bos);
+            byte[] blackAndWhiteImageBytes = bos.toByteArray();
+            return "https://transformed.url/" + id;
+        }
+
+        public static String getTransformedImageUrl(String id) {
+            return "https://transformed.url/" + id;
+        }
+    }
+}
