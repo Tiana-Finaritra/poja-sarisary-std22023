e3895c7ace5b3e5260462989c062114b59aa573c
Tiana-Finaritra <hei.finaritra.3@gmail.com>
2024-02-22 12:28:32 +0300

    fix: black and white controlleur test

--- src/test/java/hei/school/sarisary/endpoint/rest/controller/BlackAndWhiteControllerTest.java
+++ src/test/java/hei/school/sarisary/endpoint/rest/controller/BlackAndWhiteControllerTest.java
@@ -1,7 +1,13 @@
 package hei.school.sarisary.endpoint.rest.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
 import hei.school.sarisary.endpoint.rest.controller.health.BlackAndWhiteController;
 import hei.school.sarisary.endpoint.rest.controller.health.BlackAndWhiteController.ConvertBlackWhite;
+import java.io.IOException;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,20 +17,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-import java.io.IOException;
-import java.util.Map;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class BlackAndWhiteControllerTest {
 
-    @Mock
-    ConvertBlackWhite convertBlackWhite;
+    @Mock ConvertBlackWhite convertBlackWhite;
 
-    @InjectMocks
-    BlackAndWhiteController blackAndWhiteController;
+    @InjectMocks BlackAndWhiteController blackAndWhiteController;
 
     @Test
     void testUploadImage_Success() throws IOException {
@@ -33,7 +32,8 @@ public class BlackAndWhiteControllerTest {
         byte[] content = {0x42, 0x4D}; // Example content of a BMP file header
         MultipartFile file = new MockMultipartFile("file", "test.bmp", "image/bmp", content);
         String expectedTransformedImageUrl = "https://transformed.url/" + id;
-        when(convertBlackWhite.convertToBlackAndWhite(any(byte[].class), any(String.class))).thenReturn(expectedTransformedImageUrl);
+        when(convertBlackWhite.convertToBlackAndWhite(any(byte[].class), any(String.class)))
+                .thenReturn(expectedTransformedImageUrl);
 
         // When
         ResponseEntity<Void> responseEntity = blackAndWhiteController.uploadImage(id, file);
@@ -48,7 +48,8 @@ public class BlackAndWhiteControllerTest {
         String id = "123";
         byte[] content = {0x42, 0x4D}; // Example content of a BMP file header
         MultipartFile file = new MockMultipartFile("file", "test.bmp", "image/bmp", content);
-        when(convertBlackWhite.convertToBlackAndWhite(any(byte[].class), any(String.class))).thenThrow(new IOException());
+        when(convertBlackWhite.convertToBlackAndWhite(any(byte[].class), any(String.class)))
+                .thenThrow(new IOException());
 
         // When
         ResponseEntity<Void> responseEntity = blackAndWhiteController.uploadImage(id, file);
@@ -62,7 +63,8 @@ public class BlackAndWhiteControllerTest {
         // Given
         String id = "123";
         String expectedTransformedImageUrl = "https://transformed.url/" + id;
-        when(convertBlackWhite.getTransformedImageUrl(any(String.class))).thenReturn(expectedTransformedImageUrl);
+        when(convertBlackWhite.getTransformedImageUrl(any(String.class)))
+                .thenReturn(expectedTransformedImageUrl);
 
         // When
         ResponseEntity<Map<String, String>> responseEntity = blackAndWhiteController.getImageUrls(id);
